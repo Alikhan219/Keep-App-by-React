2924fb1ec670f5b4fbc3928a09e0e6c675089257
unknown <merabhai786786@gmail.com>
2020-10-18 04:15:05 -0700

    creating the header component and styling it

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <h1 className="logo">KK</h1>
+                <h1 className="logo-text">Khan Keep
+                </h1>
+
+            </div>
+        </>
+    )
+}
+export default Header;
