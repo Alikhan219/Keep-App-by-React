59e286a451333a2de07f927c65007c4e0311d6ef
unknown <merabhai786786@gmail.com>
2020-10-19 06:47:32 -0700

    creating the Title and note div and installing ui material icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
+import CreateNote from "./CreateNote"
 const App=()=>{
 return(
   <>
     <Header />
     <Footer />
+    <CreateNote />
   </>
 )
 }
