f498344d1266b39da91deab25e54e74488c2f040
unknown <merabhai786786@gmail.com>
2020-10-19 07:23:22 -0700

    adding some styles to the note div

--- src/CreateNote.jsx
+++ src/CreateNote.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
-const CreateNote=()=>{
+const CreateNote =()=>{
     return(
         <>
            <div className="main-note">
