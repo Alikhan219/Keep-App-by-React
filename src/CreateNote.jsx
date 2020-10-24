82b991774ecde73b527e6f96bce7a4de740e8ac2
unknown <merabhai786786@gmail.com>
2020-10-24 04:58:14 -0700

    adding the prevvalue method and spread method

--- src/CreateNote.jsx
+++ src/CreateNote.jsx
@@ -2,25 +2,42 @@ import React, { useState } from "react"
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
-const CreateNote =()=>{
+const CreateNote = () => {
 
 
-    const [note, setNote]= useState({
-        title:'',
-        content:""
+    const [note, setNote] = useState({
+        title: '',
+        content: ""
     })
-
-    return(
+     const inputEvent=(event)=>{
+         const value= event.target.value;
+         setNote((prevData)=>{
+             return{
+                 ...prevData, 
+             }
+         })
+     }
+    return (
         <>
-           <div className="main-note">
-      <form>
-          <input type="text" placeholder="Title" autoComplete="off" />
-          <textarea rows="" column="" placeholder="Write a note..."></textarea>
-          <Button id="add-btn">
-         <AddIcon id="add-icon"></AddIcon>
-          </Button>
-      </form>
-           </div>
+            <div className="main-note">
+                <form>
+                    <input
+                         name="title"
+                        type="text"
+                        value={note.title}
+                        onChange={inputEvent} placeholder="Title" autoComplete="off"
+                    />
+                    <textarea
+                        name="content"
+                        rows=""
+                        column=""
+                        value={note.title}
+                        onChange={inputEvent} placeholder="Write a note..."></textarea>
+                    <Button id="add-btn">
+                        <AddIcon id="add-icon"></AddIcon>
+                    </Button>
+                </form>
+            </div>
         </>
     )
 }
